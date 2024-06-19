4330614a6a6ec82fb5257e12f0ccbaad6f609b3a
sudo-m3zim <nizilplay2004@gmail.com>
2024-06-20 02:43:49 +0300

    Fixed WeaponStateMachine naming

--- Prefabs/Weapons/weapon_state.gd
+++ Prefabs/Weapons/weapon_state.gd
@@ -3,9 +3,15 @@ extends RefCounted
 
 var target: Weapon
 var target_animation_player: AnimationPlayer
-var state_machine: WeaponsStateMachine
+var state_machine: WeaponStateMachine
 
-func enter_state(target: Weapon, state_machine: WeaponsStateMachine) -> void:
+func enter_state(target: Weapon, state_machine: WeaponStateMachine) -> void:
 	self.target = target
 	self.state_machine = state_machine
 	target_animation_player = target.animation_player
+
+func update_state(delta: float) -> void:
+	pass
+
+func exit_state() -> void:
+	pass
--- Prefabs/Weapons/weapons_state_machine.gd
+++ Prefabs/Weapons/weapons_state_machine.gd
@@ -1,5 +1,8 @@
 extends Node
-class_name WeaponsStateMachine
+class_name WeaponStateMachine
 
 func _process(delta: float) -> void:
+	pass
+
+func change_state(new_state: WeaponState) -> void:
 	pass
